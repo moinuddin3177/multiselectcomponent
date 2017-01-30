93ebd617ce88d04ea3e4f9c692784563b426f9c5
MohammedKhaja <mohammmo@ds.ad.adp.com>
2017-01-30 17:49:11 +0530

    first checkin

---
+++ componentsPractical/prodTable.component.js
@@ -0,0 +1,36 @@
+﻿(function () {
+
+    var module = angular.module("productPract");
+
+
+    function fetchProducts($http) {
+        return $http.get("product.json").then(function (response) {
+            return response.data;
+        })
+    }
+
+
+    function prodTablecontroller($http) {
+        var model = this;
+        model.products = [];
+
+        model.$onInit = function () {
+            fetchProducts($http).then(function (products) {
+                model.products = products;
+            })
+            //  moin.products = [1,2,3];
+
+        }
+    }
+
+    module.component("prodTable", {
+        templateUrl: "prodTable.component.html",
+        controllerAs: "model",
+        controller: ["$http", prodTablecontroller]
+
+    });
+
+
+
+
+}());
---
+++ componentsPractical/products.js
@@ -0,0 +1,88 @@
+﻿(function () {
+
+    var module = angular.module("productPract", []);
+
+
+    function fetchProducts($http) {
+        return $http.get("product.json").then(function (response) {
+            return response.data;
+        })
+    }
+
+
+    function prodListcontroller($http) {
+        var moin = this;
+        moin.products = [];
+        moin.selectedArr = [];
+        moin.model =[];
+
+        moin.$onInit = function () {
+            fetchProducts($http).then(function (products) {
+                moin.products = products;
+                for (i = 0; i < moin.products; i++) {
+                    moin.model[i] = false;
+                }
+                
+            })
+          //  moin.products = [1,2,3];
+
+        }
+        moin.select = function (val) {
+            moin.selectedArr.push(val);
+            //console.log(moin.selectedArr);
+        }
+
+        moin.deselect = function (val) {
+            var i = moin.selectedArr.indexOf(val);
+            if (i > -1) {
+                moin.selectedArr.splice(i, 1);
+                //console.log(moin.selectedArr);
+            }
+           
+        }
+
+        moin.selectAll = function () {
+            for ( var i = 0; i < moin.products.length; i++) {
+                if(!moin.model[i]){
+                    moin.model[i] = true;
+                moin.select(moin.products[i].productName);
+            }
+            }
+            //console.log(moin.model);
+        }
+
+        moin.deselectAll = function () {
+            for (var i = 0; i < moin.products.length; i++) {
+                if (moin.model[i]) {
+                    moin.model[i] = false;
+                    moin.deselect(moin.products[i].productName);
+                }
+            }
+
+        }
+
+        moin.Itemclicked = function (val,index) {
+           // console.log(val +" "+index);
+            moin.model[index] = !moin.model[index];
+            //console.log(moin.model);
+            if (moin.model[index] ) {
+                moin.select(val);
+            } else {
+                moin.deselect(val);
+            }
+        }
+
+        
+       
+
+        }
+
+    module.component("prodList", {
+       
+        templateUrl: "prodList.component.html",
+        controllerAs: "moin",
+        controller: ["$http", prodListcontroller]
+    });
+
+
+}());
